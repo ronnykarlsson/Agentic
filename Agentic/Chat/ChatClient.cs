3949cb3f7dea2a85ac636d1e8e4a5bb46c1f8c25
ronnykarlsson <ronny.karlsson@gmail.com>
2024-02-25 14:09:00 +0100

    Refactor base ChatClient class

---
+++ Agentic/Chat/ChatClient.cs
@@ -0,0 +1,122 @@
+﻿using Agentic.Exceptions;
+using Agentic.Tools;
+using Agentic.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Agentic.Chat
+{
+    public abstract class ChatClient<TRequest> : IChatClient
+    {
+        protected string SystemMessage;
+        protected ITool[] Tools;
+
+        public int MaxTokens { get; }
+
+        protected abstract TRequest CreateRequest();
+        protected abstract void AddRequestMessage(TRequest request, ChatMessage message);
+        public abstract Task<ChatMessage> SendRequestAsync(TRequest request);
+
+        protected ChatClient(int maxTokens)
+        {
+            MaxTokens = maxTokens;
+        }
+
+        public virtual async Task<ChatMessage> SendAsync(ChatContext context)
+        {
+            if (string.IsNullOrWhiteSpace(SystemMessage))
+            {
+                throw new InvalidOperationException($"System message must be set, call {nameof(SetSystemMessage)} first.");
+            }
+
+            var request = CreateRequest();
+
+            var toolsSystemMessage = ChatHelpers.CreateDefaultSystemMessage(SystemMessage, Tools);
+            var systemMessage = new ChatMessage(Role.System, toolsSystemMessage);
+            AddRequestMessage(request, systemMessage);
+
+            var tokens = MaxTokens - CalculateTokens(systemMessage);
+            AddRequestMessages(context, request, tokens);
+
+            var chatMessage = await SendRequestAsync(request).ConfigureAwait(false);
+
+            return chatMessage;
+        }
+
+        public virtual async Task<ChatMessage> SendAsync(ChatContext context, ChatMessage message)
+        {
+            if (string.IsNullOrWhiteSpace(SystemMessage))
+            {
+                throw new InvalidOperationException($"System message must be set, call {nameof(SetSystemMessage)} first.");
+            }
+
+            // Calculate max tokens with system message tokens
+            var toolsSystemMessage = ChatHelpers.CreateDefaultSystemMessage(SystemMessage, Tools);
+            var systemMessage = new ChatMessage(Role.System, toolsSystemMessage);
+            var tokens = MaxTokens - CalculateTokens(systemMessage);
+
+            // Calculate max tokens including user message
+            var messageTokens = CalculateTokens(message);
+            tokens = tokens - messageTokens;
+            if (tokens < 0) throw new ChatException("Message is too long");
+
+            var request = CreateRequest();
+
+            AddRequestMessage(request, systemMessage);
+            AddRequestMessages(context, request, tokens);
+            AddRequestMessage(request, message);
+
+            var chatMessage = await SendRequestAsync(request).ConfigureAwait(false);
+
+            return chatMessage;
+        }
+
+        public void SetSystemMessage(string systemMessage)
+        {
+            if (string.IsNullOrWhiteSpace(systemMessage))
+            {
+                throw new ArgumentException($"'{nameof(systemMessage)}' cannot be null or whitespace.", nameof(systemMessage));
+            }
+
+            SystemMessage = systemMessage;
+        }
+
+        public void SetTools(ITool[] tools)
+        {
+            Tools = tools;
+        }
+
+        private void AddRequestMessages(ChatContext context, TRequest request, int tokens)
+        {
+            var addMessages = new List<ChatMessage>();
+
+            var tokensLeft = tokens;
+            for (int i = context.Messages.Count - 1; i >= 0; i--)
+            {
+                var chatMessage = context.Messages[i];
+
+                tokensLeft -= CalculateTokens(chatMessage);
+                if (tokensLeft <= 0) break;
+
+                addMessages.Add(chatMessage);
+            }
+
+            for (int i = addMessages.Count - 1; i >= 0; i--)
+            {
+                var chatMessage = addMessages[i];
+                AddRequestMessage(request, chatMessage);
+            }
+        }
+
+        protected virtual int CalculateTokens(ChatMessage chatMesssage)
+        {
+            return CalculateTokens(chatMesssage.Role.ToString()) + CalculateTokens(chatMesssage.Content);
+        }
+
+        protected virtual int CalculateTokens(string message)
+        {
+            return TokenHelpers.CalculateTokenCount(message);
+        }
+    }
+}
--- Agentic/Chat/OpenAIChatClient.cs
+++ Agentic/Chat/OpenAIChatClient.cs
@@ -1,7 +1,6 @@
 ﻿using Agentic.Clients.OpenAI;
 using Agentic.Exceptions;
 using Agentic.Tools;
-using Agentic.Utilities;
 using Microsoft.Extensions.Configuration;
 using System;
 using System.Linq;
@@ -10,101 +9,30 @@ using System.Threading.Tasks;
 
 namespace Agentic.Chat
 {
-    public class OpenAIChatClient : IOpenAIChatClient
+    public class OpenAIChatClient : ChatClient<OpenAIRequest>, IOpenAIChatClient
     {
         private readonly IOpenAIClient _client;
-        private string _systemMessage;
         private readonly string _model;
-        private readonly int _maxTokens;
-        private ITool[] _tools;
 
         public OpenAIChatClient(IOpenAIClient client, IConfiguration configuration)
+            : base(int.Parse(configuration["OpenAI:MaxTokens"] ?? "7000"))
         {
             if (client == null) throw new ArgumentNullException(nameof(client));
             if (configuration == null) throw new ArgumentNullException(nameof(configuration));
 
             _client = new OpenAIClient(configuration, null);
             _model = configuration["OpenAI:Model"] ?? "gpt-4-turbo-preview";
-            _maxTokens = int.Parse(configuration["OpenAI:MaxTokens"] ?? "7000");
         }
 
         public OpenAIChatClient(IOpenAIClient client, string model, int maxTokens, params ITool[] tools)
+            : base(maxTokens)
         {
             _client = client;
             _model = model;
-            _maxTokens = maxTokens;
-            _tools = tools;
+            Tools = tools;
         }
 
-        public void SetSystemMessage(string systemMessage)
-        {
-            if (string.IsNullOrWhiteSpace(systemMessage))
-            {
-                throw new ArgumentException($"'{nameof(systemMessage)}' cannot be null or whitespace.", nameof(systemMessage));
-            }
-
-            _systemMessage = systemMessage;
-        }
-
-        public void SetTools(ITool[] tools)
-        {
-            _tools = tools;
-        }
-
-        public async Task<ChatMessage> SendAsync(ChatContext context)
-        {
-            if (string.IsNullOrWhiteSpace(_systemMessage))
-            {
-                throw new InvalidOperationException($"System message must be set, call {nameof(SetSystemMessage)} first.");
-            }
-
-            var request = new OpenAIRequest(_model, _maxTokens);
-            var toolsSystemMessage = ChatHelpers.CreateDefaultSystemMessage(_systemMessage, _tools);
-            request.AddMessage(Role.System.ToString().ToLowerInvariant(), toolsSystemMessage);
-
-            AddRequestMessages(context, request, _maxTokens);
-
-            var chatMessage = await InternalSendAsync(request).ConfigureAwait(false);
-
-            return chatMessage;
-        }
-
-        public async Task<ChatMessage> SendAsync(ChatContext context, ChatMessage message)
-        {
-            var request = new OpenAIRequest(_model, _maxTokens);
-            var toolsSystemMessage = ChatHelpers.CreateDefaultSystemMessage(_systemMessage, _tools);
-            request.AddMessage(Role.System.ToString().ToLowerInvariant(), toolsSystemMessage);
-
-            var messageTokens = TokenHelpers.CalculateTokenCount(message.Content) + 1;
-            var maxTokens = _maxTokens - messageTokens;
-            if (maxTokens < 0) throw new ChatException("Message is too long");
-
-            AddRequestMessages(context, request, maxTokens);
-            request.AddMessage(Role.User.ToString().ToLowerInvariant(), message.Content);
-
-            var chatMessage = await InternalSendAsync(request).ConfigureAwait(false);
-
-            return chatMessage;
-        }
-
-        private void AddRequestMessages(ChatContext context, OpenAIRequest request, int maxTokens)
-        {
-            // Insert messages until max tokens is reached
-            var messagePosition = request.Messages.Count;
-            for (int i = context.Messages.Count - 1; i >= 0; i--)
-            {
-                var contextMessage = context.Messages[i];
-                request.Messages.Insert(messagePosition, new OpenAIMessage(contextMessage.Role.ToString().ToLowerInvariant(), contextMessage.Content));
-
-                if (CalculateTokens(request) > maxTokens)
-                {
-                    context.Messages.RemoveAt(messagePosition);
-                    break;
-                }
-            }
-        }
-
-        private async Task<ChatMessage> InternalSendAsync(OpenAIRequest request)
+        public override async Task<ChatMessage> SendRequestAsync(OpenAIRequest request)
         {
             OpenAIResponse response;
             try
@@ -121,15 +49,14 @@ namespace Agentic.Chat
             return chatMessage;
         }
 
-        private int CalculateTokens(OpenAIRequest request)
+        protected override OpenAIRequest CreateRequest()
         {
-            var tokens = 0;
-            foreach (var message in request.Messages)
-            {
-                tokens += 1; // Role token
-                tokens += TokenHelpers.CalculateTokenCount(message.Content);
-            }
-            return tokens;
+            return new OpenAIRequest(_model, MaxTokens);
+        }
+
+        protected override void AddRequestMessage(OpenAIRequest request, ChatMessage message)
+        {
+            request.AddMessage(message.Role.ToString().ToLowerInvariant(), message.Content);
         }
     }
 }
